900815a7c475edd47764c8e5025a323f4c7b7868
nomestack <nomestack@gmail.com>
2024-02-05 13:40:09 -0300

    Added more annotations and changed the way the button is rendered

--- components/Button.lua
+++ components/Button.lua
@@ -2,37 +2,40 @@ local Component = require 'components/Component'
 local Stylebox  = require 'components/Stylebox'
 local TextStyle = require 'components/TextStyle'
 
----@class Button: Component
----@operator call: Button
-local Button    = Component:extend()
+---@class ButtonSettings: ComponentSettings
+---@field text? string Represents the text content of this Button.
+---@field stylebox? Stylebox Represents the Stylebox that will be rendered behind the text.
+---@field textstyle? TextStyle Represents the TextStyle that will be rendered on top of the background.
+
 local EMPTY     = {}
 
----@class ButtonSettings: ComponentSettings
----@field text string
----@field stylebox Stylebox
----@field textstyle TextStyle
-
----@param settings? ButtonSettings
-function Button:new(settings)
+---Creates a new Button.
+---A button is just a combination of a Stylebox and a TextStyle.
+---Every other event is inherited from Component.
+---@param settings? ButtonSettings A table containing the settings of the Button. This argument can be nil.
+return function(settings)
     settings = settings or EMPTY
-    self.super.new(self, settings)
-    self.text = settings.text or "Undefined"
-    self.stylebox = settings.stylebox or Stylebox({
+
+    ---@class Button: Component
+    local Button = Component(settings)
+
+    Button.text = settings.text or "Undefined"
+    Button.stylebox = settings.stylebox or Stylebox({
         color = { 0.25, 0.25, 0.25, 1 },
         shrink = 8,
         radius = 8,
         border = 4,
         borderColor = { 0.05, 0.05, 0.05, 1 }
     })
-    self.textstyle = settings.textstyle or TextStyle({
+    Button.textstyle = settings.textstyle or TextStyle({
         color = { 0.75, 0.75, 0.75, 1 }
     })
-end
 
-function Button:draw()
-    self:capture()
-    self.stylebox:draw(self.x, self.y, self.width, self.height)
-    self.textstyle:draw(self.text, self.x, self.y, self.width, self.height)
-end
+    Button:addEventListener("draw", function(self, ...)
+        self:capture()
+        self.stylebox:draw(self.x, self.y, self.width, self.height)
+        self.textstyle:draw(self.text, self.x, self.y, self.width, self.height)
+    end)
 
-return Button
+    return Button
+end
