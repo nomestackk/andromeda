bf7a88477e751d63158530833dcf73dac9d9efbb
nomestack <nomestack@gmail.com>
2024-02-05 14:45:58 -0300

    Created the start menu page

---
+++ pages/AppStartMenu.lua
@@ -0,0 +1,18 @@
+local Page = require "components/Page"
+local HDiv = require "components/HDiv"
+local Button = require "components/Button"
+
+return function()
+    ---@class AppStartMenu: Page
+    local AppStartMenu = Page({ root = HDiv() })
+
+    AppStartMenu.playButton = AppStartMenu.root:addImmutable(Button({
+        text = "Play"
+    }))
+    AppStartMenu.buildButton = AppStartMenu.root:addImmutable(Button({
+        text = "Build"
+    }))
+    AppStartMenu.root:align()
+
+    return AppStartMenu
+end
