05c275c467ffc6ecb787886c633b8fdebcafe5a5
nomestack <nomestack@gmail.com>
2024-02-05 14:44:40 -0300

    Added save utility file

---
+++ save.lua
@@ -0,0 +1,28 @@
+local ser = require 'libraries/ser'
+local SettingsFilePath = 'settings.lua'
+local ProjectsFolderPath = 'projects/'
+
+function AndromedaNewSave()
+    AppSettings = {
+        uiScale = 1
+    }
+    love.filesystem.createDirectory(ProjectsFolderPath)
+end
+
+function AndromedaSave()
+    love.filesystem.write(SettingsFilePath, ser(AppSettings))
+end
+
+function AndromedaLoad()
+    AppSettings = loadstring(love.filesystem.read(SettingsFilePath))()
+end
+
+function AndromedaInitialize()
+    local info = love.filesystem.getInfo(SettingsFilePath)
+    if not info then
+        AndromedaNewSave()
+        AndromedaSave()
+    else
+        AndromedaLoad()
+    end
+end
